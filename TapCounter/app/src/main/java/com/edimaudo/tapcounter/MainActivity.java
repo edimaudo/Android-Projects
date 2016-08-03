2ffe0eb4da76b292f337830c254a35520e539830
Edima Udo <edimaudo@gmail.com>
2016-08-02 20:52:04 -0400

    TapCounter

--- TapCounter/app/src/main/java/com/edimaudo/tapcounter/MainActivity.java
+++ TapCounter/app/src/main/java/com/edimaudo/tapcounter/MainActivity.java
@@ -1,13 +1,42 @@
 package com.edimaudo.tapcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  private TextView counterText;
+  private Button reset, tap;
+  int count = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    counterText = (TextView) findViewById(R.id.counterText);
+    reset = (Button) findViewById(R.id.reset);
+    tap = (Button) findViewById(R.id.tap);
+
+    reset.setOnClickListener(this);
+    tap.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+
+    int choice = view.getId();
+    switch (choice){
+      case R.id.reset:
+        count = 0;
+        break;
+      case R.id.tap:
+        count++;
+        break;
+    }
+    counterText.setText(String.valueOf(count));
   }
 }
