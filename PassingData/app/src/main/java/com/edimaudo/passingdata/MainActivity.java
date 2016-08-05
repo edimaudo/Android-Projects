e7c609fe0f025d45b431e6e637e214b7fa1dda47
Edima Udo <edimaudo@gmail.com>
2016-08-05 18:55:34 -0400

    PassingData

--- PassingData/app/src/main/java/com/edimaudo/passingdata/MainActivity.java
+++ PassingData/app/src/main/java/com/edimaudo/passingdata/MainActivity.java
@@ -3,13 +3,10 @@ package com.edimaudo.passingdata;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,18 +16,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-
     userInput = (EditText) findViewById(R.id.userInput);
-    userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-      @Override
-      public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        if (actionId == EditorInfo.IME_ACTION_DONE) {
-          // do your stuff here
-        }
-        return false;
-      }
-    });
 
 
   }
@@ -63,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
       Intent in = new Intent(this, secondActivity.class);
       in.putExtra(EXTRA_MESSAGE,userInputData);
       startActivity(in);
+      userInput.setText(userInputData);
     }
   }
 
