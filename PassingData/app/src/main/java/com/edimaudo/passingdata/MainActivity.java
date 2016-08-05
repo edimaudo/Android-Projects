8cdefb80b535e533762ffcf187142e77c8aa2bf5
Edima Udo <edimaudo@gmail.com>
2016-08-05 14:36:47 -0400

    PassingData
    Passing data using intents and customizing the action bar.

---
+++ PassingData/app/src/main/java/com/edimaudo/passingdata/MainActivity.java
@@ -0,0 +1,65 @@
+package com.edimaudo.passingdata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+  private EditText userInput;
+  public final static String EXTRA_MESSAGE = "com.edimaudo.passingdata.MESSAGE";
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    userInput = (EditText) findViewById(R.id.userInput);
+    userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+      @Override
+      public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+          // do your stuff here
+        }
+        return false;
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    //getMenuInflater().inflate(R.menu.main_menu, menu);
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+
+
+      default:
+        break;
+    }
+
+    return true;
+  }
+
+  public void showActivity(){
+    String userInputData = userInput.getText().toString();
+    if (userInputData.equals("")){
+      Toast.makeText(getApplicationContext(), "Please enter some text", Toast.LENGTH_SHORT).show();
+    } else {
+      Intent in = new Intent(this, secondActivity.class);
+      in.putExtra(EXTRA_MESSAGE,userInputData);
+      startActivity(in);
+    }
+  }
+
+
+}
---
+++ PassingData/app/src/main/java/com/edimaudo/passingdata/secondActivity.java
@@ -0,0 +1,20 @@
+package com.edimaudo.passingdata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class secondActivity extends AppCompatActivity {
+  private TextView textView;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+
+    textView = (TextView) findViewById(R.id.textView);
+    Intent in = getIntent();
+    String message = in.getStringExtra(MainActivity.EXTRA_MESSAGE);
+    textView.setText(message);
+  }
+}
