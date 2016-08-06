f2760b3caae4c0538f62a1b5453b58ec86356914
Edima Udo <edimaudo@gmail.com>
2016-08-06 18:25:08 -0400

    ChangeDateTime
    Update the date and time using a date picker and then show the output
    in a textView.

---
+++ ChangeDateTime/app/src/main/java/com/edimaudo/changedatetime/MainActivity.java
@@ -0,0 +1,19 @@
+package com.edimaudo.changedatetime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+  private TextView textView;
+  private Button changeDate;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    textView = (TextView) findViewById(R.id.textView);
+    
+  }
+}
