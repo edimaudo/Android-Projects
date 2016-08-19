9db2459152278f7bf9b9c179ea8a8fbbee4b2e72
Edima Udo <edimaudo@gmail.com>
2016-08-19 16:24:41 -0400

    AddPhoto
    Add photo to an image view.

--- AddPhoto/app/src/main/java/com/edimaudo/addphoto/MainActivity.java
+++ AddPhoto/app/src/main/java/com/edimaudo/addphoto/MainActivity.java
@@ -1,13 +1,81 @@
 package com.edimaudo.addphoto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+  private ImageView photo;
+  private static int RESULT_LOAD_IMAGE = 1;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    photo = (ImageView) findViewById(R.id.photo);
   }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return super.onCreateOptionsMenu(menu);
+
+  }
+
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_add:
+        isStoragePermissionGranted();
+        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+        break;
+      default:
+        break;
+    }
+
+    return true;
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+      Uri selectedImage = data.getData();
+      String[] filePathColumn = { MediaStore.Images.Media.DATA };
+      Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+      cursor.moveToFirst();
+      int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+      String picturePath = cursor.getString(columnIndex);
+      cursor.close();
+      photo.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+    }
+  }
+
+  public boolean isStoragePermissionGranted() {
+    if (Build.VERSION.SDK_INT >= 23) {
+      if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+              == PackageManager.PERMISSION_GRANTED) {
+        return true;
+      } else {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+
+
 }
