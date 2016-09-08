d54c18f2715a4c6d2f6ec07f209083c983986a35
Edima Udo <edimaudo@gmail.com>
2016-09-07 20:33:15 -0400

    QuickWords

--- QuickWords/app/src/main/java/com/edimaudo/quickwords/MainActivity.java
+++ QuickWords/app/src/main/java/com/edimaudo/quickwords/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -66,108 +70,137 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     setButtonLetter();
 
-
   }
+
+  public void updateButtonViewInfo(View view){
+    view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
+    view.setEnabled(false);
+    showWord();
+  }
+  
   @Override
   public void onClick(View view) {
+    scoreUpdate.setText("");
     switch (view.getId()){
       case R.id.button1:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
-        viewData.add(button1);
         wordBuilder.append(button1.getText());
+        viewData.add(button1);
+        updateButtonViewInfo(view);
         break;
       case R.id.button2:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
-        viewData.add(button1);
+        viewData.add(button2);
         wordBuilder.append(button2.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button3:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button3);
         wordBuilder.append(button3.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button4:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button4);
         wordBuilder.append(button4.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button5:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button5);
         wordBuilder.append(button5.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button6:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button6);
         wordBuilder.append(button6.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button7:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button7);
         wordBuilder.append(button7.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button8:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button8);
         wordBuilder.append(button8.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.button9:
-        view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorAccent));
         viewData.add(button9);
         wordBuilder.append(button9.getText());
+        updateButtonViewInfo(view);
         break;
       case R.id.cancelButton:
-        clearSelectedLetters("cancel");
+        clearSelectedLetter("cancel");
         break;
       case R.id.submitButton:
-        checkWord();
-        calculateWordScore();
-        showUpdate();
-        clearSelectedLetters("update");
+        updateScore();
+        clearSelectedLetter("not cancel");
         break;
     }
-    showWord();
 
   }
-
 
   public void setButtonLetter(){
      for(int button = 0; button < buttonList.size() ;button++){
         Random rand = new Random();
         buttonList.get(button).setText(String.valueOf(alphabet[rand.nextInt(alphabet.length)]));
     }
-
-  }
-  public void showUpdate(){
-    //winWord.append("Correct Points! ");
   }
 
-  public void clearSelectedLetters(String update){
-    wordSelected.setText("");
-    wordBuilder = new StringBuilder();
-    if (update.equals("cancel")){
-        for(int button = 0; button < viewData.size();button++){
-          viewData.get(button).setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
-        }
+  public void updateScore(){
+    int currentScore = 0;
+    String output = "";
+    if(checkWord(wordBuilder.toString())){
+      currentScore = wordBuilder.toString().length() * wordBuilder.toString().length();
+      winWord.append("Correct ");
+      winWord.append(currentScore);
+      winWord.append(" points");
+      output = winWord.toString();
     } else {
-      for(int button = 0; button < viewData.size();button++){
+      output = incorrectWord;
+    }
+    score+=currentScore;
+    scoreUpdate.setText(output);
+    scoreInfo.setText(String.valueOf("Score: " + score));
+
+  }
+
+  public void clearSelectedLetter(String update){
+    for(int button = 0; button < viewData.size();button++){
+      if (!update.equals("cancel")){
         Random rand = new Random();
-        viewData.get(button).setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
         viewData.get(button).setText(String.valueOf(alphabet[rand.nextInt(alphabet.length)]));
       }
+      viewData.get(button).setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimary));
+      viewData.get(button).setEnabled(true);
     }
-  }
-
-  public void checkWord(){
-}
-
-  public void calculateWordScore(){
+    viewData = new ArrayList<Button>();
+    wordSelected.setText("");
+    wordBuilder.setLength(0);
 
   }
+
+  public boolean checkWord(String word) {
+    boolean output = false;
+    try {
+      InputStream is = getResources().openRawResource(R.raw.words);
+      BufferedReader br = new BufferedReader(new InputStreamReader(is));
+      String line = br.readLine();
+      while(line != null){
+        if(line.equals(word.toLowerCase())){
+          output = true;
+          break;
+        }
+        line = br.readLine();
+      }
+      br.close();
+      is.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return output;
+  }
+
 
   public void showWord(){
     wordSelected.setText(wordBuilder.toString());
   }
-
-
 }
