ea4cf35f0d599e11fbedfe3fbb114b5076c807a3
Edima Udo <edimaudo@gmail.com>
2016-09-13 15:07:09 -0400

    MiWok

--- MiWok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ MiWok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,11 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view){
+        Intent intent = new Intent(this,NumbersActivity.class);
+        startActivity(intent);
     }
 }
