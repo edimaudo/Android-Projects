e3f93ee0498c8aa74676db9f2581c41e1323f907
Edima Udo <edimaudo@gmail.com>
2016-09-13 12:25:59 -0400

    MiWok

--- MiWok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ MiWok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
     }
 }
