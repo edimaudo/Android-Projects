815a0908411ac08cd9523db0db0dbf2c8327a391
Edima Udo <edimaudo@gmail.com>
2016-09-21 22:38:40 -0400

    pets

---
+++ pets/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -0,0 +1,55 @@
+package com.example.android.pets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * {@link PetCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of pet data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
+public class PetCursorAdapter extends CursorAdapter {
+
+  /**
+   * Constructs a new {@link PetCursorAdapter}.
+   *
+   * @param context The context
+   * @param c       The cursor from which to get the data.
+   */
+  public PetCursorAdapter(Context context, Cursor c) {
+    super(context, c, 0 /* flags */);
+  }
+
+  /**
+   * Makes a new blank list item view. No data is set (or bound) to the views yet.
+   *
+   * @param context app context
+   * @param cursor  The cursor from which to get the data. The cursor is already
+   *                moved to the correct position.
+   * @param parent  The parent to which the new view is attached to
+   * @return the newly created list item view.
+   */
+  @Override
+  public View newView(Context context, Cursor cursor, ViewGroup parent) {
+    // TODO: Fill out this method and return the list item view (instead of null)
+    return null;
+  }
+
+  /**
+   * This method binds the pet data (in the current row pointed to by cursor) to the given
+   * list item layout. For example, the name for the current pet can be set on the name TextView
+   * in the list item layout.
+   *
+   * @param view    Existing view, returned earlier by newView() method
+   * @param context app context
+   * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+   *                correct row.
+   */
+  @Override
+  public void bindView(View view, Context context, Cursor cursor) {
+    // TODO: Fill out this method
+  }
+}
--- pets/app/src/main/java/com/example/android/pets/data/PetContract.java
+++ pets/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,8 +1,8 @@
 package com.example.android.pets.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
-
 /**
  * Created by edima on 2016-09-13.
  */
@@ -19,7 +19,14 @@ public class PetContract {
   private PetContract(){}
 
   public static final class PetEntry implements BaseColumns{
+    public static final String CONTENT_LIST_TYPE =
+            ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
 
+    /**
+     * The MIME type of the {@link #CONTENT_URI} for a single pet.
+     */
+    public static final String CONTENT_ITEM_TYPE =
+            ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
     public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
     public final static String TABLE_NAME = "pets";
     public final static String _ID = BaseColumns._ID;
@@ -31,7 +38,16 @@ public class PetContract {
     public static final int GENDER_UNKNOWN = 0;
     public static final int GENDER_MALE = 1;
     public static final int GENDER_FEMALE = 2;
+
+    public static boolean isValidGender(int gender) {
+      if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE) {
+        return true;
+      }
+      return false;
+    }
   }
+
+
 
 
 }
