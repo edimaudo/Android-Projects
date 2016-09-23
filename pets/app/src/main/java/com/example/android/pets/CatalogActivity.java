e92f479512f573dc26abdb9ed884c5e27e4e0045
Edima Udo <edimaudo@gmail.com>
2016-09-22 21:53:37 -0400

    pets

--- pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.pets;
 
 import android.content.ContentValues;
@@ -22,32 +8,28 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-
+import android.widget.ListView;
 import com.example.android.pets.data.PetContract.PetEntry;
 import com.example.android.pets.data.PetDBHelper;
-
-import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_BREED;
-import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_GENDER;
-import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_NAME;
-import static com.example.android.pets.data.PetContract.PetEntry.COLUMN_PET_WEIGHT;
-import static com.example.android.pets.data.PetContract.PetEntry._ID;
 
 /**
  * Displays list of pets that were entered and stored in the app.
  */
-public class CatalogActivity extends AppCompatActivity {
-    PetDBHelper mDbHelper;
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int PET_LOADER = 0;
+    PetCursorAdapter mCursorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-
-
 
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -58,6 +40,15 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ListView petListView = (ListView) findViewById(R.id.list);
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
+        mCursorAdapter = new PetCursorAdapter(this,null);
+        petListView.setAdapter(mCursorAdapter);
+
+        getLoaderManager().initLoader(PET_LOADER,null,this);
     }
 
     @Override
@@ -74,9 +65,9 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                // Do nothing for now
-                //insertPet();
-                //displayDatabaseInfo();
+
+                insertPet();
+
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
@@ -103,34 +94,23 @@ public class CatalogActivity extends AppCompatActivity {
         //long newRowId = db.insert(TABLE_NAME, null, values);
     }
 
-    private void displayDatabaseInfo() {
-        // To access our database, we instantiate our subclass of SQLiteOpenHelper
-
-        // Create and/or open a database to read from it
-        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        String[] projection = {
-                _ID,
-                COLUMN_PET_NAME,
-                COLUMN_PET_BREED,
-                COLUMN_PET_GENDER,
-                COLUMN_PET_WEIGHT
-        };
 
 
-        Cursor cursor = getContentResolver().query(PetEntry.CONTENT_URI,projection,null,null,null);
 
 
-        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-
-        cursor.close();
-
-
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {PetEntry._ID,PetEntry.COLUMN_PET_NAME,PetEntry.COLUMN_PET_BREED};
+        return new CursorLoader(this, PetEntry.CONTENT_URI,projection,null,null,null);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
--- pets/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ pets/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,13 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract;
 
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
@@ -35,7 +39,8 @@ public class PetCursorAdapter extends CursorAdapter {
   @Override
   public View newView(Context context, Cursor cursor, ViewGroup parent) {
     // TODO: Fill out this method and return the list item view (instead of null)
-    return null;
+
+    return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
   }
 
   /**
@@ -51,5 +56,16 @@ public class PetCursorAdapter extends CursorAdapter {
   @Override
   public void bindView(View view, Context context, Cursor cursor) {
     // TODO: Fill out this method
+    TextView nameTextView = (TextView) view.findViewById(R.id.name);
+    TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+
+    int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+    int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+
+    String petName = cursor.getString(nameColumnIndex);
+    String petBreed = cursor.getString(breedColumnIndex);
+
+    nameTextView.setText(petName);
+    summaryTextView.setText(petBreed);
   }
 }
