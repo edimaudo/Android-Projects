3aaf3cccc141a58aad3b0e13767ef3336c628b1c
Edima Udo <edimaudo@gmail.com>
2016-10-12 10:25:58 -0400

    Barcode Detection

--- BarcodeDetection/app/src/main/java/com/edimaudo/barcodedetection/MainActivity.java
+++ BarcodeDetection/app/src/main/java/com/edimaudo/barcodedetection/MainActivity.java
@@ -4,28 +4,57 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ImageView myImageView;
+  private BarcodeDetector detector;
+  private Button btn;
+  private TextView txtView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Button btn = (Button) findViewById(R.id.button);
+    txtView = (TextView) findViewById(R.id.txtContent);
+
+    btn = (Button) findViewById(R.id.button);
     btn.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        myImageView = (ImageView) findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        myImageView.setImageBitmap(myBitmap);
+
+        detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){
+          txtView.setText("Could not set up the detector!");
+          return;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+        txtView.setText(thisCode.rawValue);
       }
     });
 
-    ImageView myImageView = (ImageView) findViewById(R.id.imgview);
-    Bitmap myBitmap = BitmapFactory.decodeResource(
-            getApplicationContext().getResources(),
-            R.drawable.puppy);
-    myImageView.setImageBitmap(myBitmap);
+
   }
 }
