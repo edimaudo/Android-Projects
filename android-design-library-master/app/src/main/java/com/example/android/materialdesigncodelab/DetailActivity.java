8cdaa0b7ef46e0264735b742d81f0eafc5fd2a42
Edima Udo <edimaudo@gmail.com>
2016-10-13 19:17:30 -0400

    android design

---
+++ android-design-library-master/app/src/main/java/com/example/android/materialdesigncodelab/DetailActivity.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialdesigncodelab;
+
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Provides UI for the Detail page with Collapsing Toolbar.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_POSITION = "position";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Set Collapsing Toolbar layout to the screen
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        // Set title of Detail page
+        // collapsingToolbar.setTitle(getString(R.string.item_title));
+
+        int postion = getIntent().getIntExtra(EXTRA_POSITION, 0);
+        Resources resources = getResources();
+        String[] places = resources.getStringArray(R.array.places);
+        collapsingToolbar.setTitle(places[postion % places.length]);
+
+        String[] placeDetails = resources.getStringArray(R.array.place_details);
+        TextView placeDetail = (TextView) findViewById(R.id.place_detail);
+        placeDetail.setText(placeDetails[postion % placeDetails.length]);
+
+        String[] placeLocations = resources.getStringArray(R.array.place_locations);
+        TextView placeLocation =  (TextView) findViewById(R.id.place_location);
+        placeLocation.setText(placeLocations[postion % placeLocations.length]);
+
+        TypedArray placePictures = resources.obtainTypedArray(R.array.places_picture);
+        ImageView placePicutre = (ImageView) findViewById(R.id.image);
+        placePicutre.setImageDrawable(placePictures.getDrawable(postion % placePictures.length()));
+
+        placePictures.recycle();
+    }
+}
