8502dfa16266f640ee4327680467a28ee4b2217b
Edima Udo <edimaudo@gmail.com>
2016-10-13 23:29:13 -0400

    LightsOut

--- LightsOut/app/src/main/java/com/edimaudo/lightsout/MainActivity.java
+++ LightsOut/app/src/main/java/com/edimaudo/lightsout/MainActivity.java
@@ -1,9 +1,11 @@
 package com.edimaudo.lightsout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   image12,image13,image14,image15,image16;
 
   int[] lightArray = new int[16];
+  int[] imageViewArray = {
+          R.id.image1, R.id.image2,R.id.image3,R.id.image4,
+          R.id.image5,R.id.image6,R.id.image7,R.id.image8,
+          R.id.image9,R.id.image10,R.id.image11,R.id.image12,
+          R.id.image13,R.id.image14,R.id.image15,R.id.image16};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -60,59 +67,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   public void playGame(){
     boolean gameEnd = false;
     while(!gameEnd){
-
+      
     }
 
   }
 
   public void resetGame(){
-
+    for (int i = 0; i < lightArray.length; i++) {
+      lightArray[i] = 0;
+    }
   }
 
   public void assignLight(){
+    Random rand = new Random();
+    for (int i = 0; i < lightArray.length; i++){
+      lightArray[i] = rand.nextInt(2);
+      if (lightArray[i] == 1){
+        ImageView img = (ImageView) findViewById(imageViewArray[i]);
+        img.setImageResource(R.drawable.circle_change);
+      }
+    }
 
   }
 
-  public void changeColor(View view){
-
+  public void changeColor(int view, int position, int score){
+    ImageView imageView = (ImageView) findViewById(view);
+    if(score == 0){
+      lightArray[position] = 1;
+      imageView.setImageResource(R.drawable.circle_change);
+    } else {
+      lightArray[position] = 0;
+      imageView.setImageResource(R.drawable.circle);
+    }
   }
 
   @Override
   public void onClick(View view) {
+    int output = 0;
     switch(view.getId()){
       case R.id.image1:
         break;
       case R.id.image2:
+        output = 1;
         break;
       case R.id.image3:
+        output = 2;
         break;
       case R.id.image4:
+        output = 3;
         break;
       case R.id.image5:
+        output = 4;
         break;
       case R.id.image6:
+        output = 5;
         break;
       case R.id.image7:
+        output = 6;
         break;
       case R.id.image8:
+        output = 7;
         break;
       case R.id.image9:
+        output = 8;
         break;
       case R.id.image10:
+        output = 9;
         break;
       case R.id.image11:
+        output = 10;
         break;
       case R.id.image12:
+        output = 11;
         break;
       case R.id.image13:
+        output = 12;
         break;
       case R.id.image14:
+        output = 13;
         break;
       case R.id.image15:
+        output = 14;
         break;
       case R.id.image16:
+        output = 15;
         break;
     }
+
+    changeColor(view.getId(),output,lightArray[output]);
   }
 
 
