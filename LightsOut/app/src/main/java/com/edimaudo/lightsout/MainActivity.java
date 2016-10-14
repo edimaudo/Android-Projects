ffd825fda9a2e791b4f31a79d50656154e9aea03
Edima Udo <edimaudo@gmail.com>
2016-10-14 16:53:20 -0400

    LightsOut

--- LightsOut/app/src/main/java/com/edimaudo/lightsout/MainActivity.java
+++ LightsOut/app/src/main/java/com/edimaudo/lightsout/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -61,50 +62,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     image15.setOnClickListener(this);
     image16.setOnClickListener(this);
 
-    gameInfo();
+    //assignLight();
+    //checkEndGame();
 
   }
 
-  public void playGame(){
-    boolean gameEnd = false;
-    while(!gameEnd){
+  //check if the game should end
+  public void checkEndGame(){
       if(arraySum(lightArray) == 0){
-        gameEnd = true;
-      } else {
-
+        AlertDialog.Builder winBuild = new AlertDialog.Builder(this);
+        winBuild.setTitle("Yay!");
+        winBuild.setMessage("Congrats! you won!");
+        winBuild.setPositiveButton("Play Again?",
+                new DialogInterface.OnClickListener() {
+                  public void onClick(DialogInterface dialog, int id) {
+                    resetGame();
+                  }
+                });
+        winBuild.setNegativeButton("Exit",
+                new DialogInterface.OnClickListener() {
+                  public void onClick(DialogInterface dialog, int id) {
+                    MainActivity.this.finish();
+                  }
+                });
+        winBuild.show();
       }
-
-      }
-    AlertDialog.Builder winBuild = new AlertDialog.Builder(this);
-    winBuild.setTitle("Yay!");
-    winBuild.setMessage("Congrats! you won!");
-    winBuild.setPositiveButton("Play Again?",
-            new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int id) {
-                resetGame();
-              }
-            });
-    winBuild.setNegativeButton("Exit",
-            new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int id) {
-                MainActivity.this.finish();
-              }
-            });
-    winBuild.show();
     }
 
-  public void gameInfo(){
-    assignLight();
-    playGame();
-  }
 
+
+
+  //set all light array values to zero
   public void resetGame(){
     for (int i = 0; i < lightArray.length; i++) {
       lightArray[i] = 0;
     }
-    gameInfo();
+    assignLight();
   }
 
+  // sum light array
   public int arraySum(int [] arrayInfo){
     int output = 0;
     for (int i = 0; i < arrayInfo.length; i++){
@@ -113,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     return output;
   }
 
+  //assign light colors randomly
   public void assignLight(){
     Random rand = new Random();
     for (int i = 0; i < lightArray.length; i++){
@@ -125,71 +122,105 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
   }
 
-  public void changeColor(int view, int position, int score){
-    ImageView imageView = (ImageView) findViewById(view);
-    if(score == 0){
-      lightArray[position] = 1;
-      imageView.setImageResource(R.drawable.circle_change);
-    } else {
-      lightArray[position] = 0;
-      imageView.setImageResource(R.drawable.circle);
+
+
+  //list.indexOf('e')
+  public void updateColor(ArrayList<Integer> arrayList){
+    for (int i = 0; i < arrayList.size(); i++){
+      ImageView imageView = (ImageView) findViewById(arrayList.get(i));
+
+      if(lightArray[i] == 0){
+        imageView.setImageResource(R.drawable.circle_change);
+        lightArray[i] = 1;
+      } else {
+        imageView.setImageResource(R.drawable.circle);
+        lightArray[i] = 0;
+      }
     }
+
+
   }
+
+
 
   @Override
   public void onClick(View view) {
-    int output = 0;
+
     switch(view.getId()){
       case R.id.image1:
+
         break;
       case R.id.image2:
-        output = 1;
+
         break;
       case R.id.image3:
-        output = 2;
+
         break;
       case R.id.image4:
-        output = 3;
+
         break;
       case R.id.image5:
-        output = 4;
+
         break;
       case R.id.image6:
-        output = 5;
+
         break;
       case R.id.image7:
-        output = 6;
+
         break;
       case R.id.image8:
-        output = 7;
+
         break;
       case R.id.image9:
-        output = 8;
+
         break;
       case R.id.image10:
-        output = 9;
+
         break;
       case R.id.image11:
-        output = 10;
+
         break;
       case R.id.image12:
-        output = 11;
+
         break;
       case R.id.image13:
-        output = 12;
+
         break;
       case R.id.image14:
-        output = 13;
+
         break;
       case R.id.image15:
-        output = 14;
+
         break;
       case R.id.image16:
-        output = 15;
+
         break;
     }
 
-    changeColor(view.getId(),output,lightArray[output]);
+      //updateColor(output);
+    //changeColor(view.getId(),output,lightArray[output]);
+  }
+
+  public int[] generateArray(int value){
+    int[] outputArray;
+    if (value == 1 || value == 4 || value == 13 || value == 16){
+      outputArray = new int[3];
+    } else if (value == 6 || value == 7 || value == 10 || value == 11) {
+      outputArray = new int[5];
+    } else {
+      outputArray = new int[4];
+    }
+
+    switch (value){
+      case 1:
+        outputArray[0] = R.id.image1;
+        outputArray[1] = R.id.image2;
+        outputArray[2] = R.id.image5;
+        break;
+    }
+
+
+    return outputArray;
   }
 
 
