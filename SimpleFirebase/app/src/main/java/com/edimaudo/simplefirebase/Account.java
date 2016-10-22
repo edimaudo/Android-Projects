9875b6ed444b08963b7d2726b413fe59821e4923
Edima Udo <edimaudo@gmail.com>
2016-10-22 19:17:27 -0400

    SimpleFirebase

--- SimpleFirebase/app/src/main/java/com/edimaudo/simplefirebase/Account.java
+++ SimpleFirebase/app/src/main/java/com/edimaudo/simplefirebase/Account.java
@@ -1,13 +1,39 @@
 package com.edimaudo.simplefirebase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Account extends AppCompatActivity {
+  private Button logOut;
+  private FirebaseAuth mAuth;
+  private FirebaseAuth.AuthStateListener mAuthStateListener;
 
-  @Override
+
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_account);
+    mAuth = FirebaseAuth.getInstance();
+    mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+      @Override
+      public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        if(firebaseAuth.getCurrentUser() == null){
+          startActivity(new Intent(Account.this,MainActivity.class));
+        }
+      }
+    };
+
+    logOut = (Button) findViewById(R.id.logOut);
+    logOut.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mAuth.signOut();
+      }
+    });
   }
 }
