280900c5745eca0c8d3602304a5beb0024071872
Edima Udo <edimaudo@gmail.com>
2016-10-23 14:49:12 -0400

    PicoFermiBagel

--- PicoFermiBagel/app/src/main/java/com/edimaudo/picofermibagel/game.java
+++ PicoFermiBagel/app/src/main/java/com/edimaudo/picofermibagel/game.java
@@ -1,7 +1,10 @@
 package com.edimaudo.picofermibagel;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,17 +18,18 @@ public class game extends AppCompatActivity {
   private Button submitGuess;
   private EditText userInput;
   int number1, number2, number3;
-  int guessNum = 0;
+  int guessNum = generateGameNumber();
+
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game);
-
+    Log.i("guess",String.valueOf(guessNum));
     textOutput = (TextView) findViewById(R.id.textOutput);
 
-    submitGuess = (Button) findViewById(R.id.guessButton);
+    submitGuess = (Button) findViewById(R.id.submitGuess);
 
     userInput = (EditText) findViewById(R.id.userInput);
 
@@ -34,18 +38,39 @@ public class game extends AppCompatActivity {
       public void onClick(View view) {
           if (userInput.getText().toString().isEmpty()){
             Toast.makeText(game.this, "Please enter a number", Toast.LENGTH_SHORT).show();
-          } else if (userInput.getText().toString().length() > 3) {
+          } else if (userInput.getText().toString().length() != 3) {
             Toast.makeText(game.this, "Please enter a 3 digit number", Toast.LENGTH_SHORT).show();
           }else {
-
+            String userData = userInput.getText().toString();
+            checkGameWin(userData);
           }
+        userInput.setText("");
       }
     });
 
   }
 
-  public void checkGameWin(){
-
+  public void checkGameWin(String userData){
+    if(Integer.parseInt(userData) == guessNum){
+      AlertDialog.Builder winBuild = new AlertDialog.Builder(this);
+      winBuild.setTitle("Yay!");
+      winBuild.setMessage("Congrats! you guess the correct number!");
+      winBuild.setPositiveButton("Play Again?",
+              new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                  resetGame();
+                }
+              });
+      winBuild.setNegativeButton("Exit",
+              new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                  game.this.finish();
+                }
+              });
+      winBuild.show();
+    } else {
+      textOutput.setText(generateBagelOutcome(userData));
+    }
   }
 
   public int generateGameNumber(){
@@ -53,7 +78,35 @@ public class game extends AppCompatActivity {
     return random.nextInt(889) + 111;
   }
 
-  public String generateBagelOutcome(){
-    return "";
+  public void resetGame(){
+    guessNum = generateGameNumber();
+    Log.i("guess",String.valueOf(guessNum));
+  }
+
+  public String generateBagelOutcome(String userData){
+
+    StringBuilder stringInfo = new StringBuilder();
+    String[] userDataArray = userData.split("");
+    String[] guessDataArray = String.valueOf(guessNum).split("");
+    for (int i = 0; i < userDataArray.length;i++){
+      stringInfo.append(checkOutcome(userDataArray[i], guessDataArray));
+      stringInfo.append("\n");
+    }
+    //Log.i("count", userData + String.valueOf(guessNum) + stringInfo.toString());
+    return stringInfo.toString();
+  }
+
+  public String checkOutcome(String value, String[] arrayInfo){
+    String outcome = "";
+
+    if (value.equals(arrayInfo[0])){
+      outcome = "Fermi";
+    } else if (value.equals(arrayInfo[1]) || value.equals(arrayInfo[2])){
+      outcome = "Pico";
+    } else {
+      outcome = "Bagel";
+    }
+
+    return outcome;
   }
 }
