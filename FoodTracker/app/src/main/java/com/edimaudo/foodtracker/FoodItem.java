14504d911958729e0e00f40b64edc651929cd7e1
Edima Udo <edimaudo@gmail.com>
2016-10-24 10:47:33 -0400

    FoodTracker

---
+++ FoodTracker/app/src/main/java/com/edimaudo/foodtracker/FoodItem.java
@@ -0,0 +1,17 @@
+package com.edimaudo.foodtracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.edimaudo.foodtracker.R;
+
+public class FoodItem extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_food_item);
+
+  }
+
+}
--- FoodTracker/app/src/main/java/com/edimaudo/foodtracker/MainActivity.java
+++ FoodTracker/app/src/main/java/com/edimaudo/foodtracker/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Intent in = new Intent(getApplicationContext(), food_info.class);
+        Intent in = new Intent(getApplicationContext(), FoodItem.class);
         startActivity(in);
       }
     });
