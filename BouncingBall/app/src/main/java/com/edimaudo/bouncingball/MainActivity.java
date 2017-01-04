3898cd9dd68d37626f90bb333e3f3151fb6bb35e
Edima Udo <edimaudo@gmail.com>
2017-01-04 16:11:28 -0500

    Bouncing Ball
    Updated main activity

--- BouncingBall/app/src/main/java/com/edimaudo/bouncingball/MainActivity.java
+++ BouncingBall/app/src/main/java/com/edimaudo/bouncingball/MainActivity.java
@@ -1,13 +1,16 @@
 package com.edimaudo.bouncingball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+  private ImageView ball;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ball = (ImageView) findViewById(R.id.ball);
   }
 }
