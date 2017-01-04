78c4f79a96729bab7bdd92f4dda626951cad5af1
Edima Udo <edimaudo@gmail.com>
2017-01-04 17:01:23 -0500

    Bouncing Ball
    Updated manifest screen orientation

--- BouncingBall/app/src/main/java/com/edimaudo/bouncingball/MainActivity.java
+++ BouncingBall/app/src/main/java/com/edimaudo/bouncingball/MainActivity.java
@@ -2,6 +2,8 @@ package com.edimaudo.bouncingball;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ball = (ImageView) findViewById(R.id.ball);
+
+    
+
+    ball.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        //do something
+      }
+    });
+
+    ball.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+      }
+    });
   }
 }
