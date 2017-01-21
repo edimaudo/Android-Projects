580e8bae70eba7d3b7f7d8838bb6e82ee4dc3fc1
Edima Udo <edimaudo@gmail.com>
2017-01-21 15:42:23 -0500

    Pikture

---
+++ Pikture/app/src/main/java/com/edimaudo/pikture/activity/pictures.java
@@ -0,0 +1,15 @@
+package com.edimaudo.pikture.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.edimaudo.pikture.R;
+
+public class pictures extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_pictures);
+  }
+}
