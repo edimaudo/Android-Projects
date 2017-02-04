c70c11f4c24e5a9835549e010111c9600f8f22dd
Edima Udo <edimaudo@gmail.com>
2017-02-03 22:07:52 -0500

    Simon

--- Simon/app/src/main/java/com/edimaudo/simon/MainActivity.java
+++ Simon/app/src/main/java/com/edimaudo/simon/MainActivity.java
@@ -1,13 +1,26 @@
 package com.edimaudo.simon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+  private Button playButton;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    playButton = (Button) findViewById(R.id.playButton);
+
+    playButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, game.class);
+        startActivity(intent);
+      }
+    });
   }
 }
