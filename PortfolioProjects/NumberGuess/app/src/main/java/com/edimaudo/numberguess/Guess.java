578e0061b17b8bcecf89d40fa70a505ac0c43b58
Edima Udo <edimaudo@gmail.com>
2017-05-10 12:33:23 -0400

    NumberGuess
    A simple number guessing game

---
+++ PortfolioProjects/NumberGuess/app/src/main/java/com/edimaudo/numberguess/Guess.java
@@ -0,0 +1,14 @@
+package com.edimaudo.numberguess;
+
+/**
+ * Created by edima on 2017-05-10.
+ */
+
+//Todo: create guess class;
+  //Todo: map out game outline
+//Todo: create game UI
+  //Todo: work on game activity
+  //Todo: create tests
+
+public class Guess {
+}
