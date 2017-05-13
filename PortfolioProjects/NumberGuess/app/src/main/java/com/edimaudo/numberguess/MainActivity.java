2bdad852a12ad5b37812534d95f14133fa933732
Edima Udo <edimaudo@gmail.com>
2017-05-13 13:06:39 -0400

    numberGuess

--- PortfolioProjects/NumberGuess/app/src/main/java/com/edimaudo/numberguess/MainActivity.java
+++ PortfolioProjects/NumberGuess/app/src/main/java/com/edimaudo/numberguess/MainActivity.java
@@ -1,9 +1,12 @@
 package com.edimaudo.numberguess;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+  public Button gamePlay, gameScore;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
