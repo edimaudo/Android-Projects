0430452f693e409e81095810ed9a586254662761
Edima Udo <edimaudo@gmail.com>
2017-06-19 09:57:26 -0400

    Reminders
    Added plan for development, fab icons, strings and style

--- Reminders/app/src/main/java/com/edimaudo/reminders/MainActivity.java
+++ Reminders/app/src/main/java/com/edimaudo/reminders/MainActivity.java
@@ -1,29 +1,30 @@
 package com.edimaudo.reminders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity {
+  FloatingActionButton fab;
+  ExpandableListView lvExp;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    lvExp = (ExpandableListView) findViewById(R.id.lvExp);
+    fab = (FloatingActionButton) findViewById(R.id.fab);
+
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+        startActivity(new Intent(MainActivity.this,AddReminder.class));
       }
     });
   }
