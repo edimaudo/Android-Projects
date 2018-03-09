794dee81211e5367ca49da1ce1793315d0127ec3
Edima Udo <edimaudo@gmail.com>
2018-03-09 15:46:06 -0500

    MainActivity

--- TimeZoneConverter/app/src/main/java/com/edimaudo/timezoneconverter/MainActivity.java
+++ TimeZoneConverter/app/src/main/java/com/edimaudo/timezoneconverter/MainActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  SeekBar seekBar = findViewById(R.id.seekBar);
+  TextView userTime = findViewById(R.id.userTime);
+
+  
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
