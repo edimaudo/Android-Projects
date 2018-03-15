5aed3f2b2ef8ce537277e05424564f816929fe4a
Edima Udo <edimaudo@gmail.com>
2018-03-15 11:13:28 -0400

    main Activity

--- TimeZoneConverter/app/src/main/java/com/edimaudo/timezoneconverter/MainActivity.java
+++ TimeZoneConverter/app/src/main/java/com/edimaudo/timezoneconverter/MainActivity.java
@@ -1,19 +1,43 @@
 package com.edimaudo.timezoneconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
   SeekBar seekBar = findViewById(R.id.seekBar);
-  TextView userTime = findViewById(R.id.userTime);
+  final TextView userTime = findViewById(R.id.userTime);
 
-  
+
+
+
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
+    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
+        userTime.setText((seekBar.getProgress() < 10 ? "0"
+                + Integer.toString(seekBar.getProgress()) : Integer.toString(seekBar.getProgress())) + ":00");
+        //userTime.setText(Integer.toString(seekBar.getProgress()));
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
   }
 }
