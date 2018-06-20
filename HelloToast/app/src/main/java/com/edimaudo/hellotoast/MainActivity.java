9fa19c6bc8f72126c62c5c5a2a1a31e3450d578c
Edima Udo <edimaudo@gmail.com>
2018-06-20 09:21:50 -0400

    hello toast

--- HelloToast/app/src/main/java/com/edimaudo/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/edimaudo/hellotoast/MainActivity.java
@@ -1,15 +1,21 @@
 package com.edimaudo.hellotoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+  public Button countbutton;
+  
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    View countbutton = findViewById(R.id.count_button);
 
   }
 }
