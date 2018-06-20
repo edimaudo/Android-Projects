d05713d1b4c768b3918743655206e1f2210a8bc5
Edima Udo <edimaudo@gmail.com>
2018-06-20 18:26:22 -0400

    cleaned up main activity

--- HelloToast/app/src/main/java/com/edimaudo/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/edimaudo/hellotoast/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-  public Button countbutton;
+  public Button countButton, toastButton;
+  public TextView countText;
+  public int textCount = 0;
 
 
   @Override
@@ -15,7 +19,25 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    View countbutton = findViewById(R.id.count_button);
+    countButton = (Button) findViewById(R.id.count_button);
+    countText = (TextView) findViewById(R.id.count_text);
+    toastButton = (Button) findViewById(R.id.toast_button);
+
+    countButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        textCount+=1;
+        if (countText != null) countText.setText(Integer.toString(textCount));
+        
+      }
+    });
+
+    toastButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+      }
+    });
 
   }
 }
