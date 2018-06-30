21277163b26e2632508ad1c099643bd3f7600ddf
Edima Udo <edimaudo@gmail.com>
2018-06-29 22:04:17 -0400

    updated onImageFromCameraClick with code

--- FaceDetection2/app/src/main/java/com/edimaudo/facedetection/MainActivity.java
+++ FaceDetection2/app/src/main/java/com/edimaudo/facedetection/MainActivity.java
@@ -1,11 +1,21 @@
 package com.edimaudo.facedetection;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-  
+
+  private static final int REQUEST_IMAGE_CAPTURE = 1;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -14,6 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
 
   public void onImageFromCameraClick(View view) {
+    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (intent.resolveActivity(getPackageManager()) != null) {
+      File photoFile = null;
+      try {
+        photoFile = createImageFile();
+      } catch (IOException e) {
+        Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+      }
 
+      if (photoFile != null) {
+        Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+      }
+    }
   }
 }
