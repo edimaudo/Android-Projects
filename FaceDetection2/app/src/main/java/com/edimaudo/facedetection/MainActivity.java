ef70194c67abc99966e4be3f6bfb1e9b736e4e92
Edima Udo <edimaudo@gmail.com>
2018-06-29 21:11:17 -0400

    fixed function name

--- FaceDetection2/app/src/main/java/com/edimaudo/facedetection/MainActivity.java
+++ FaceDetection2/app/src/main/java/com/edimaudo/facedetection/MainActivity.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
   }
 
-  public void onImageFromClick (View view){
-    
+
+  public void onImageFromCameraClick(View view) {
+
   }
 }
