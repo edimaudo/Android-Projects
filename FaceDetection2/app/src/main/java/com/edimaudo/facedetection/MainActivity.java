0ca558979f0c456c2620cfa4c17ae95f95da85e7
Edima Udo <edimaudo@gmail.com>
2018-06-30 11:51:31 -0400

    added createImageFile function

--- FaceDetection2/app/src/main/java/com/edimaudo/facedetection/MainActivity.java
+++ FaceDetection2/app/src/main/java/com/edimaudo/facedetection/MainActivity.java
@@ -1,9 +1,14 @@
 package com.edimaudo.facedetection;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,10 +16,16 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
   private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+  String mCurrentPhotoPath;
+
+  private static final int REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 2;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,28 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
       }
     }
+  }
+
+  private File createImageFile() throws IOException {
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+      // permission not granted, initiate request
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE);
+    } else {
+      // Create an image file name
+      String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+      String imageFileName = "JPEG_" + timeStamp + "_";
+      File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+      File image = File.createTempFile(
+              imageFileName,  /* prefix */
+              ".jpg",         /* suffix */
+              storageDir      /* directory */
+      );
+      mCurrentPhotoPath = image.getAbsolutePath(); // save this to use in the intent
+
+      return image;
+    }
+
+    return null;
   }
 
 
