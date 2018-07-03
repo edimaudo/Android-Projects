467d7a56f6fb3bed790f9f25244cb988b7bef995
Edima Udo <edimaudo@gmail.com>
2018-07-02 20:24:03 -0400

    updated signIn function with code

--- likeagram/app/src/main/java/com/edimaudo/likeagram/MainActivity.java
+++ likeagram/app/src/main/java/com/edimaudo/likeagram/MainActivity.java
@@ -1,10 +1,14 @@
 package com.edimaudo.likeagram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+
 public class MainActivity extends AppCompatActivity {
+
+  private static final int RC_SIGN_IN = 123;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
   }
 
   public void signIn(View view) {
+
+    startActivityForResult(
+            // Get an instance of AuthUI based on the default app
+            AuthUI.getInstance().createSignInIntentBuilder().build(),
+            RC_SIGN_IN);
   }
 }
