10bca2edd2ada77f9c9d1edfaf4842f7ecd9dfbe
Edima Udo <edimaudo@gmail.com>
2018-08-01 14:18:46 -0400

    Updated the main activity file with empty methods for the three actions

--- ImplicitIntents/app/src/main/java/com/edimaudo/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/edimaudo/implicitintents/MainActivity.java
@@ -2,6 +2,7 @@ package com.edimaudo.implicitintents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void openWebsite(View view) {
+  }
+
+  public void openLocation(View view) {
+  }
+
+  public void shareText(View view) {
   }
 }
