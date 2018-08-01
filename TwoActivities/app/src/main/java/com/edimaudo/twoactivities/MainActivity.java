16a178cfdfb55d2e4dd51a1daee0ba750988f177
Edima Udo <edimaudo@gmail.com>
2018-08-01 14:11:50 -0400

    Created ImplicitIntents app

--- TwoActivities/app/src/main/java/com/edimaudo/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/edimaudo/twoactivities/MainActivity.java
@@ -19,13 +19,75 @@ public class MainActivity extends AppCompatActivity {
   private TextView mReplyHeadTextView;
   private TextView mReplyTextView;
 
+
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+      outState.putBoolean("reply_visible", true);
+      outState.putString("reply_text", mReplyTextView.getText().toString());
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(LOG_TAG, "onStop");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(LOG_TAG, "onDestroy");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(LOG_TAG, "onPause");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(LOG_TAG, "onResume");
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    Log.d(LOG_TAG, "onRestart");
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    Log.d(LOG_TAG, "-------");
+    Log.d(LOG_TAG, "onCreate");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mMessageEditText = (EditText) findViewById(R.id.editText_main);
     mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
     mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
+
+    // Restore the saved state. See onSaveInstanceState() for what gets saved.
+    if (savedInstanceState != null) {
+      boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+      // Show both the header and the message views. If isVisible is
+      // false or missing from the bundle, use the default layout.
+      if (isVisible) {
+        mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+        mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+        mReplyTextView.setVisibility(View.VISIBLE);
+      }
+    }
+  }
+
+  @Override
+  public void onStart(){
+    super.onStart();
+    Log.d(LOG_TAG, "onStart");
   }
 
 
--- TwoActivities/app/src/main/java/com/edimaudo/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/edimaudo/twoactivities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.edimaudo.twoactivities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,9 @@ public class SecondActivity extends AppCompatActivity {
 
   public static final String EXTRA_REPLY =
           "com.twoactivities.extra.REPLY";
+
+  private static final String LOG_TAG =
+          SecondActivity.class.getSimpleName();
 
   private EditText mReply;
 
@@ -27,11 +31,42 @@ public class SecondActivity extends AppCompatActivity {
     textView.setText(message);
   }
 
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(LOG_TAG, "onStop");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(LOG_TAG, "onDestroy");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(LOG_TAG, "onPause");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(LOG_TAG, "onResume");
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    Log.d(LOG_TAG, "onRestart");
+  }
+
   public void returnReply(View view) {
     String reply = mReply.getText().toString();
     Intent replyIntent = new Intent();
     replyIntent.putExtra(EXTRA_REPLY, reply);
     setResult(RESULT_OK, replyIntent);
+    Log.d(LOG_TAG, "End SecondActivity");
     finish();
   }
 }
