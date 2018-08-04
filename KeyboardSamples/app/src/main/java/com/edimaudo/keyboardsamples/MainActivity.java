684c5e5d6a7bcab4a01d644c7a4b59400793b05c
Edima Udo <edimaudo@gmail.com>
2018-08-04 12:46:57 -0400

    Updated Keyboard samples

--- KeyboardSamples/app/src/main/java/com/edimaudo/keyboardsamples/MainActivity.java
+++ KeyboardSamples/app/src/main/java/com/edimaudo/keyboardsamples/MainActivity.java
@@ -1,7 +1,10 @@
 package com.edimaudo.keyboardsamples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void showText(View view) {
+    EditText editText = (EditText) findViewById(R.id.editText_main);
+    if (editText != null) {
+      String showString = editText.getText().toString();
+      Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+    }
   }
 }
