521e54672e2cd8c0bbfb29025e31b45b8b1792b8
Edima <edimaudo@gmail.com>
2022-04-14 18:53:14 -0400

    Create GameActivity.java

---
+++ Cyberardy/app/src/main/java/com/example/cyberardy/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.cyberardy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game);
+  }
+}
