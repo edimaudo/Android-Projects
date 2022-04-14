25ba876afb39593ee206f418da9f40dda5d4416f
Edima <edimaudo@gmail.com>
2022-04-14 19:37:52 -0400

    Update MainActivity.java

--- Cyberardy/app/src/main/java/com/example/cyberardy/MainActivity.java
+++ Cyberardy/app/src/main/java/com/example/cyberardy/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
