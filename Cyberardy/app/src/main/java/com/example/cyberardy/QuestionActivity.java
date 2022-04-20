77fa3d5de142ad25935436ce4529650e36e8243d
Edima <edimaudo@gmail.com>
2022-04-19 20:55:46 -0400

    Update QuestionActivity.java

--- Cyberardy/app/src/main/java/com/example/cyberardy/QuestionActivity.java
+++ Cyberardy/app/src/main/java/com/example/cyberardy/QuestionActivity.java
@@ -1,6 +1,7 @@
 package com.example.cyberardy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -29,5 +30,9 @@ public class QuestionActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_question);
+
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
   }
 }
