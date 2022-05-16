d6f026abc05dc106cc2bdd403f53382a5e15d895
Edima <edimaudo@gmail.com>
2022-05-16 16:41:21 -0400

    Update ExampleUnitTest.java

--- DiceRoller/app/src/test/java/com/edimaudo/diceroller/ExampleUnitTest.java
+++ DiceRoller/app/src/test/java/com/edimaudo/diceroller/ExampleUnitTest.java
@@ -8,8 +8,13 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+//  @Test
+//  public void addition_isCorrect() throws Exception {
+//    assertEquals(5, 2 + 3);
+//  }
   @Test
-  public void addition_isCorrect() throws Exception {
-    assertEquals(4, 2 + 2);
+  public void generate_number() throws Exception{
+    assertEquals();
   }
+
 }
