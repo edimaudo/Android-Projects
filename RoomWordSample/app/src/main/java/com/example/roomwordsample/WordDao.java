8ba4a29cd6f9a8085b24b136b447eb15ceabfd06
Edima <edimaudo@gmail.com>
2022-05-18 16:58:08 -0400

    Update WordDao.java

--- RoomWordSample/app/src/main/java/com/example/roomwordsample/WordDao.java
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/WordDao.java
@@ -1,4 +1,22 @@
 package com.example.roomwordsample;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
-class WordDao {
+import java.util.List;
+@Dao
+public interface WordDao {
+
+  // allowing the insert of the same word multiple times by passing a
+  // conflict resolution strategy
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  void insert(Word word);
+
+  @Query("DELETE FROM word_table")
+  void deleteAll();
+
+  @Query("SELECT * FROM word_table ORDER BY word ASC")
+  LiveData<List<Word>> getAlphabetizedWords();
 }
