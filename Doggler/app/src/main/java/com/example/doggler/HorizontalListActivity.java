55a112adc75e52a864b4ac267e6cc0c51ca0bef6
Edima <edimaudo@gmail.com>
2022-05-18 20:25:50 -0400

    Create HorizontalListActivity.java

---
+++ Doggler/app/src/main/java/com/example/doggler/HorizontalListActivity.java
@@ -0,0 +1,13 @@
+package com.example.doggler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorizontalListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
