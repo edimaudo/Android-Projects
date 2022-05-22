66b1a47a1332aad8a9c4e884d10d04ed807ca68b
Edima <edimaudo@gmail.com>
2022-05-22 18:48:36 -0400

    Create ShippingInfoActivity.java

---
+++ shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/shipping/ShippingInfoActivity.java
@@ -0,0 +1,14 @@
+package com.google.codelabs.mdc.java.shipping;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShippingInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shipping_info_activity);
+    }
+}
