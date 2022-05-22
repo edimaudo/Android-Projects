e66401c628edf510707c14c0bbd40c3a7698c310
Edima <edimaudo@gmail.com>
2022-05-22 13:43:50 -0400

    Update ProductGridFragment.java

--- shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
+++ shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -6,13 +6,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class ProductGridFragment extends Fragment {
 
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public View onCreateView(
+                @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Inflate the layout for this fragment with the ProductGrid theme
+            View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+            // Set up the toolbar
+            setUpToolbar(view);
+
+            return view;
+        }
+
+        private void setUpToolbar(View view) {
+            Toolbar toolbar = view.findViewById(R.id.app_bar);
+            AppCompatActivity activity = (AppCompatActivity) getActivity();
+            if (activity != null) {
+                activity.setSupportActionBar(toolbar);
+            }
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+            menuInflater.inflate(R.menu.shr_toolbar_menu, menu);
+            super.onCreateOptionsMenu(menu, menuInflater);
+        }
+
     }
-}
