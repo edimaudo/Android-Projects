416027f3c6e9b1010bf96402853d0e77db6ef0b5
Edima <edimaudo@gmail.com>
2022-05-23 19:33:35 -0400

    Update DogAdapter.java

--- Doggler/app/src/main/java/com/example/doggler/DogAdapter.java
+++ Doggler/app/src/main/java/com/example/doggler/DogAdapter.java
@@ -1,4 +1,34 @@
 package com.example.doggler;
+import android.view.View;
+import android.view.ViewGroup;
 
-class DogAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogCardViewHolder> {
+
+  @NonNull
+  @Override
+  public DogCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull DogCardViewHolder holder, int position) {
+
+  }
+
+  
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+
+
+  public class DogCardViewHolder extends RecyclerView.ViewHolder {
+    public DogCardViewHolder(@NonNull View itemView) {
+      super(itemView);
+    }
+  }
 }
