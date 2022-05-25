deace5adad39187d0df1c7a21bd50763642b5b20
Edima <edimaudo@gmail.com>
2022-05-24 21:10:28 -0400

    Update DogAdapter.java

--- Doggler/app/src/main/java/com/example/doggler/DogAdapter.java
+++ Doggler/app/src/main/java/com/example/doggler/DogAdapter.java
@@ -9,9 +9,14 @@ import java.util.List;
 
 public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogCardViewHolder> {
 
+  // TODO: Initialize the data using the List found in data/DataSource
   private List<Dog> dogList;
 
+  /**
+   * Initialize view elements
+   */
   public class DogCardViewHolder extends RecyclerView.ViewHolder {
+    // TODO: Declare and initialize all of the list item UI components
     public DogCardViewHolder(@NonNull View itemView) {
 
       super(itemView);
@@ -29,17 +34,39 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogCardViewHolde
             .inflate(R.layout.grid_list_item, parent, false);
 
     return new DogCardViewHolder(itemView);
+
+    // TODO: Use a conditional to determine the layout type and set it accordingly.
+    //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
+    //  the vertical/horizontal list item should be used.
+
+    // TODO Inflate the layout
+
+    // TODO: Null should not be passed into the view holder. This should be updated to reflect
+    //  the inflated layout.
+   //// return DogCardViewHolder(null)
   }
 
   @Override
   public void onBindViewHolder(@NonNull DogCardViewHolder holder, int position) {
+// TODO: Get the data at the current position
+    // TODO: Set the image resource for the current dog
+    // TODO: Set the text for the current dog's name
+    // TODO: Set the text for the current dog's age
 
+    //// val resources = context?.resources
+    // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
+    //  R.string.dog_hobbies string constant.
+    //  Passing an argument to the string resource looks like:
+    //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
   }
 
 
 
   @Override
   public int getItemCount() {
+
+    //TODO: return the size of the data set instead of 0
+
     return 0;
   }
 
