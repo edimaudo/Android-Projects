1d20a643e0932898f75df95eddb717a397497c75
Edima <edimaudo@gmail.com>
2022-05-24 21:47:51 -0400

    Update DogAdapter.java

--- Doggler/app/src/main/java/com/example/doggler/DogAdapter.java
+++ Doggler/app/src/main/java/com/example/doggler/DogAdapter.java
@@ -4,6 +4,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
 
@@ -17,9 +18,15 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogCardViewHolde
    */
   public class DogCardViewHolder extends RecyclerView.ViewHolder {
     // TODO: Declare and initialize all of the list item UI components
+    public TextView age, hobbies, name;
+    public ImageView dogImage;
     public DogCardViewHolder(@NonNull View itemView) {
-
       super(itemView);
+      dogImage = (ImageView) itemView.findViewById(R.id.imageView);
+      name = (TextView) itemView.findViewById(R.id.TextViewName);
+      age = (TextView) itemView.findViewById(R.id.TextViewAge);
+      hobbies = (TextView) itemView.findViewById(R.id.TextViewHobbies);
+
     }
   }
 
@@ -30,44 +37,46 @@ public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogCardViewHolde
   @NonNull
   @Override
   public DogCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    View itemView = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.grid_list_item, parent, false);
-
-    return new DogCardViewHolder(itemView);
-
     // TODO: Use a conditional to determine the layout type and set it accordingly.
     //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
     //  the vertical/horizontal list item should be used.
 
     // TODO Inflate the layout
+    View itemView = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.grid_list_item, parent, false);
+
 
     // TODO: Null should not be passed into the view holder. This should be updated to reflect
     //  the inflated layout.
-   //// return DogCardViewHolder(null)
+    return new DogCardViewHolder(itemView);
   }
 
   @Override
   public void onBindViewHolder(@NonNull DogCardViewHolder holder, int position) {
-// TODO: Get the data at the current position
+    // TODO: Get the data at the current position
     // TODO: Set the image resource for the current dog
     // TODO: Set the text for the current dog's name
     // TODO: Set the text for the current dog's age
 
-    //// val resources = context?.resources
     // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
     //  R.string.dog_hobbies string constant.
     //  Passing an argument to the string resource looks like:
     //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
+
+    Dog dog = dogList.get(position);
+    holder.dogImage.setImageResource(dog.getImageResourceId());
+    holder.name.setText(dog.getName());
+    holder.age.setText(dog.getAge());
+    holder.hobbies.setText(dog.getHobbies());
+
   }
 
 
 
   @Override
   public int getItemCount() {
-
-    //TODO: return the size of the data set instead of 0
-
-    return 0;
+    //TODO: return the size of the data
+    return dogList.size();
   }
 
 
