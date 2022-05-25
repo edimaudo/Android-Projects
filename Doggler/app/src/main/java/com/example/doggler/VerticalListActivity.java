625bdcfec4911a24b5b2debc72f87277a147348f
Edima <edimaudo@gmail.com>
2022-05-24 21:47:58 -0400

    Update VerticalListActivity.java

--- Doggler/app/src/main/java/com/example/doggler/VerticalListActivity.java
+++ Doggler/app/src/main/java/com/example/doggler/VerticalListActivity.java
@@ -1,14 +1,50 @@
 package com.example.doggler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import com.example.doggler.DogAdapter;
+import com.example.doggler.Layout;
+import com.example.doggler.Dog;
+import java.util.ArrayList;
+import java.util.List;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 
 public class VerticalListActivity extends AppCompatActivity {
+
+  public List<Dog> dogList = new ArrayList<>();
+  private RecyclerView recyclerView;
+  private DogAdapter dAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_vertical_list);
 
+    recyclerView = (RecyclerView) findViewById(R.id.vertical_recycler_view);
+    dAdapter = new DogAdapter(dogList);
+    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+    recyclerView.setLayoutManager(mLayoutManager);
+    recyclerView.setAdapter(dAdapter);
+    prepareData();
+
+  }
+
+  // Load information
+  private void prepareData(){
+    Dog dog= new Dog(R.drawable.tzeitel, "Tzeitel","7", "sunbathing");
+    dogList.add(dog);
+    dog= new Dog(R.drawable.leroy, "Leroy","4", "sleeping in dangerous places");
+    dogList.add(dog);
+    dog= new Dog(R.drawable.frankie, "Frankie","2", "stealing socks");
+    dogList.add(dog);
+    dog= new Dog(R.drawable.nox, "Nox","8", "meeting new animals");
+    dogList.add(dog);
+    dog= new Dog(R.drawable.faye, "Faye","8", "Digging in the garden");
+    dogList.add(dog);
+    dog= new Dog(R.drawable.bella, "Bella","14", "Chasing sea foam");
+    dogList.add(dog);
   }
 }
