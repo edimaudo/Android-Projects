06cc92521ccd5a887fcf273b4309e0a25518ebd7
Edima <edimaudo@gmail.com>
2022-05-26 15:56:41 -0400

    Update MainActivity.java

--- LunchTray/app/src/main/java/com/example/lunchtray/MainActivity.java
+++ LunchTray/app/src/main/java/com/example/lunchtray/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.lunchtray;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.fragment_start_order);
+
   }
 }
