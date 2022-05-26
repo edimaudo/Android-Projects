b7436af6f824cd4d960c08c10fd6a9f236732bad
Edima <edimaudo@gmail.com>
2022-05-26 15:56:38 -0400

    Create ItemType.java

---
+++ LunchTray/app/src/main/java/com/example/lunchtray/ItemType.java
@@ -0,0 +1,33 @@
+package com.example.lunchtray;
+
+class ItemType {
+   private int ENTREE = 1;
+   private int SIDE_DISH = 2;
+   private int ACCOMPANIMENT = 3;
+
+   public int getENTREE() {
+      return ENTREE;
+   }
+
+   public void setENTREE(int ENTREE) {
+      this.ENTREE = ENTREE;
+   }
+
+   public int getSIDE_DISH() {
+      return SIDE_DISH;
+   }
+
+   public void setSIDE_DISH(int SIDE_DISH) {
+      this.SIDE_DISH = SIDE_DISH;
+   }
+
+   public int getACCOMPANIMENT() {
+      return ACCOMPANIMENT;
+   }
+
+   public void setACCOMPANIMENT(int ACCOMPANIMENT) {
+      this.ACCOMPANIMENT = ACCOMPANIMENT;
+   }
+
+
+}
