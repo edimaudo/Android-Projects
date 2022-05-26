c097475a182b2ad9eba55551e758f9e53531455f
Edima <edimaudo@gmail.com>
2022-05-25 22:25:23 -0400

    Update Layout.java

--- Doggler/app/src/main/java/com/example/doggler/Layout.java
+++ Doggler/app/src/main/java/com/example/doggler/Layout.java
@@ -3,5 +3,21 @@ package com.example.doggler;
 class Layout {
   private final int VERTICAL = 1;
   private final int  HORIZONTAL = 2;
+
+  public Layout() {
+  }
+
   private final int GRID = 3;
+
+  public int getVERTICAL() {
+    return VERTICAL;
+  }
+
+  public int getHORIZONTAL() {
+    return HORIZONTAL;
+  }
+
+  public int getGRID() {
+    return GRID;
+  }
 }
