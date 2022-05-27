e3a903a71c18c3b14c66f52c0f5087fdb1575587
Edima <edimaudo@gmail.com>
2022-05-26 22:03:34 -0400

    Update DataSource.java

--- LunchTray/app/src/main/java/com/example/lunchtray/DataSource.java
+++ LunchTray/app/src/main/java/com/example/lunchtray/DataSource.java
@@ -1,10 +1,26 @@
 package com.example.lunchtray;
 
+import android.content.ClipData;
+
 import com.example.lunchtray.ItemType;
 import com.example.lunchtray.MenuItem;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 class DataSource {
 
-  Map<String, List<String>> hm = new HashMap<String, List<String>>();
+  //Map<String, List<String>> menuItem = new HashMap<String, List<String>>();
+  //Map<Object,ArrayList<Object>> multiMap = new HashMap<>();
+  ItemType itemType = new ItemType();
+  
+//  cauliflower
+//  MenuItem(
+//          "Cauliflower",
+//          "Whole cauliflower, brined, roasted, and deep fried",
+//          7.00,
+//          itemType.getEntree()
+//  )
+//  List<Object> imtList1 = List.of("P1", "Q1");
 
 }
