ae5c85012f944dcee8d207bcc440ba675ea1f9d8
Edima <edimaudo@gmail.com>
2022-05-31 18:39:32 -0400

    Update MainActivity.java

--- LunchTray/app/src/main/java/com/example/lunchtray/MainActivity.java
+++ LunchTray/app/src/main/java/com/example/lunchtray/MainActivity.java
@@ -9,10 +9,27 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+  public Button startOrderButton;
+  public Intent orderIntent;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.fragment_start_order);
+    setContentView(R.layout.activity_main);
 
+    startOrderButton = (Button) findViewById(R.id.start_order_btn);
+
+    startOrderButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+       launchEntree();
+      }
+    });
+  }
+
+  public void launchEntree(){
+    orderIntent = new Intent(this,EntreeMenu.class);
+    startActivity(orderIntent);
   }
 }
+
