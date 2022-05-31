30bfb83b614d1088df394da48b8035aa13b52272
Edima <edimaudo@gmail.com>
2022-05-31 18:39:27 -0400

    Create SideMenu.java

---
+++ LunchTray/app/src/main/java/com/example/lunchtray/SideMenu.java
@@ -0,0 +1,14 @@
+package com.example.lunchtray;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SideMenu extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_side_menu);
+  }
+}
