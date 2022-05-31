78a7a8d0e6a38e457371e579adfdf69a727375eb
Edima <edimaudo@gmail.com>
2022-05-31 18:39:14 -0400

    Update DataSource.java

--- LunchTray/app/src/main/java/com/example/lunchtray/DataSource.java
+++ LunchTray/app/src/main/java/com/example/lunchtray/DataSource.java
@@ -1,91 +1,79 @@
 package com.example.lunchtray;
 
-import android.content.ClipData;
-
-import com.example.lunchtray.ItemType;
-import com.example.lunchtray.MenuItem;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 
 class DataSource {
   ItemType itemType = new ItemType();
-
   int entree = itemType.getENTREE();
-
-  public int getEntree() {
-    return entree;
-  }
-
-  public int getSidedish() {
-    return sidedish;
-  }
-
-  public int getAccompaniment() {
-    return accompaniment;
-  }
-
   int sidedish = itemType.getSIDE_DISH();
   int accompaniment = itemType.getACCOMPANIMENT();
 
-  public Map<String, MenuItem> getMenuItems() {
-    return menuItems;
+
+  public List<MenuItem> entreeData() {
+    List<MenuItem> entreeItemList = new ArrayList<>();
+    MenuItem menuItem= new MenuItem("Cauliflower",
+            "Whole cauliflower, brined, roasted, and deep fried", 7.00, entree);
+    entreeItemList.add(menuItem);
+    menuItem = new MenuItem("Three Bean Chili",
+            "Black beans, red beans, kidney beans, slow cooked, topped with onion",
+            4.00, entree);
+    entreeItemList.add(menuItem);
+    menuItem = new MenuItem("Mushroom Pasta",
+            "Penne pasta, mushrooms, basil, with plum tomatoes cooked in garlic and olive oil", 5.50, entree);
+    entreeItemList.add(menuItem);
+    menuItem = new MenuItem("Spicy Black Bean Skillet",
+            "Seasonal Vegetables, black beans, house spice blend, served with avocados and pickled onions",
+            0.50, accompaniment);
+    entreeItemList.add(menuItem);
+    return entreeItemList;
+
   }
 
-  public void setMenuItems(Map<String, MenuItem> menuItems) {
-    this.menuItems = menuItems;
+  public List<MenuItem>  sideDishData(){
+    List<MenuItem> sideItemList = new ArrayList<>();
+    MenuItem menuItem= new MenuItem("Summer Salad",
+            "Heirloom tomatoes, butter lettuce, peaches, avocado, balsamic dressing",
+            2.50, sidedish);
+    sideItemList .add(menuItem);
+    menuItem = new MenuItem("Butternut Squash Soup",
+            "Roasted butternut squash, roasted peppers, chili oil",
+            3.00,sidedish);
+    sideItemList .add(menuItem);
+    menuItem = new MenuItem("Spicy Potatoes",
+            "Marble potatoes, roasted, and fried in house spice blend",
+            2.00, sidedish);
+    sideItemList .add(menuItem);
+    menuItem = new MenuItem("Coconut Rice",
+            "Rice, coconut milk, lime, and sugar",
+            1.50, sidedish);
+    sideItemList .add(menuItem);
+    return sideItemList;
   }
 
+  public List<MenuItem>  accompainmentData(){
+    List<MenuItem> itemList = new ArrayList<>();
+    MenuItem menuItem= new MenuItem("Lunch Roll",
+            "Fresh baked roll made in house",
+            0.50,
+            accompaniment);
+    itemList.add(menuItem);
+    menuItem= new MenuItem("Mixed Berries",
+            "Strawberries, blueberries, raspberries, and huckleberries",
+            1.00,
+            accompaniment);
+    itemList.add(menuItem);
+    menuItem= new MenuItem("Pickled Veggies",
+            "Pickled cucumbers and carrots, made in house",
+            0.50,
+            accompaniment);
+    itemList.add(menuItem);
+    return itemList;
 
-  Map<String, MenuItem> menuItems = Map.of(
-          "cauliflower", new MenuItem("Cauliflower",
-                  "Whole cauliflower, brined, roasted, and deep fried", 7.00, entree),
-          "chili", new MenuItem("Three Bean Chili",
-                  "Black beans, red beans, kidney beans, slow cooked, topped with onion",
-                  4.00, entree),
-          "pasta", new MenuItem("Mushroom Pasta",
-                  "Penne pasta, mushrooms, basil, with plum tomatoes cooked in garlic and " +
-                          "olive oil", 5.50, entree),
-          "salad", new MenuItem(
-                  "Summer Salad",
-                  "Heirloom tomatoes, butter lettuce, peaches, avocado, balsamic dressing",
-                  2.50,
-                  sidedish),
-          "soup", new MenuItem(
-                  "Butternut Squash Soup",
-                  "Roasted butternut squash, roasted peppers, chili oil",
-                  3.00,sidedish),
-          "potatoes", new MenuItem(
-                  "Spicy Potatoes",
-                  "Marble potatoes, roasted, and fried in house spice blend",
-                  2.00,
-                  sidedish
-          ),
-          "rice", new MenuItem(
-                  "Coconut Rice",
-                  "Rice, coconut milk, lime, and sugar",
-                  1.50,
-                  sidedish
-          ),
-          "bread", new MenuItem(
-                  "Lunch Roll",
-                  "Fresh baked roll made in house",
-                  0.50,
-                  accompaniment
-          ),
-          "berries", new MenuItem(
-                  "Mixed Berries",
-                  "Strawberries, blueberries, raspberries, and huckleberries",
-                  1.00,
-                  accompaniment
-          ),
-          "pickles", new MenuItem(
-                  "Pickled Veggies",
-                  "Pickled cucumbers and carrots, made in house",
-                  0.50,
-                  accompaniment)
-
-  );
+  }
 }
+
+
+
+
+
